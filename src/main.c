f43df5d17a30996ce64875c7ccbd45b34e2b824a
Andrei <hexahedron8@gmail.com>
2024-06-13 20:14:02 +0200

    init

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int add(int a, int b);
+
+int main() {
+    printf("Hello world");
+    int a = 1;
+    int word = 2;
+    int b = a + 1;
+    printf("hi");
+
+    printf("%d", add(1 , 2));
+}
